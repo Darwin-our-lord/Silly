0bd7de6dc3c89c3d2bf032fddfe1be60bb5c24a2
Darwin-our-lord <David@prebensen.dk>
2025-06-23 15:04:19 +0200

    basic movement

---
+++ Gerd Venere/Assets/Scripts/playerMovement.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+
+public class playerMovement : MonoBehaviour
+{
+    private float speed = 5;
+    private float jumpSpeed = 8;
+
+    bool isGrounded;
+
+    Rigidbody2D rb;
+    
+    
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float hor = Input.GetAxisRaw("Horizontal");
+        rb.linearVelocity = new Vector2 (hor * speed, rb.linearVelocity.y);
+
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded) 
+        {
+            rb.AddForce(new Vector2(0, jumpSpeed),ForceMode2D.Impulse);
+            isGrounded = false;
+        }
+
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.gameObject.CompareTag("Floor"))
+        {
+            isGrounded = true;
+        }
+    }
+}
